fc27d19a2aade7b217f1cfe06017884614356aa1
Teddy Bradsher <teddybradsher@gmail.com>
2020-07-10 00:42:01 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,6 @@
-def reverse_each_word ("Hello there, and how are you?")
-  "sentence sentence?" = ["Hello" "there", "sentence?"]
-  "sentence sentence?".each do |sentence|
-    sentence.reverse
-  end 
-  sentence
+def reverse_each_word (sentence)
+  reversed = sentence.split(" ").collect do |word|
+  word.reverse
+end
+  reversed.join(" ")
 end
