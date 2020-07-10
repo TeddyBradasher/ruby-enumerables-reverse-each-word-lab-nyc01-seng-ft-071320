ed53e99b907c179e151bc336705894fdb2745efa
Teddy Bradsher <teddybradsher@gmail.com>
2020-07-10 00:35:13 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,7 @@
 def reverse_each_word ("sentence sentence?")
   "sentence sentence?" = ["sentence", "sentence?"]
   "sentence sentence?".each do |sentence|
-    
+    sentence.reverse
+  end 
+  sentence
+end
